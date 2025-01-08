dec593141e47dc28884e062d4062167f4b7cf19b
aqil344f <maqilathalahsusanto@gmail.com>
2025-01-08 23:36:26 +0700

    Add files via upload

---
+++ qgis2web_2025_01_06-15_34_38_196470/data/ARENAOLAHRAGA_AR_50K_13.js
@@ -0,0 +1,1 @@
+var json_ARENAOLAHRAGA_AR_50K_13 = {"type":"FeatureCollection","name":"ARENAOLAHRAGA_AR_50K_13","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
